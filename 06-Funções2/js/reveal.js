a54625ba8d813e2e1628dc8d27c8df43606b3e61
diegotpereira <diegoteixeirapereira@hotmail.com>
2021-12-17 16:07:22 -0300

    criação do app javascript com exemplo de reveal.

---
+++ 06-Funções2/js/reveal.js
@@ -0,0 +1,44 @@
+const calcRevealPattern = (function() {
+
+    let n = 0
+
+    function _checkNumber(n) {
+
+        if (typeof n !== 'number') {
+
+            throw TypeError('Você precisa passar um número.')
+        }
+    }
+
+    function somar(_n) {
+
+        _checkNumber(_n)
+
+        n += _n
+
+        return this
+    }
+
+    function subtrair(_n) {
+
+        _checkNumber(_n)
+
+        n -= _n
+        return this
+    }
+
+    function log() {
+        console.log(n);
+
+        return this
+    }
+
+    return {
+        somar,
+        subtrair,
+        log
+    }
+})()
+
+calcRevealPattern.somar(5).somar('7').subtrair(2).log()
+calcRevealPattern.log()
